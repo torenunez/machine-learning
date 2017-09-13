879e7238aafec4b6b0c1a6c8c7c2d4d869c5d10b
torenunez <salvador.nunez@berkeley.edu>
2017-09-12 23:02:29 -0700

    add random action

--- projects/smartcab/smartcab/agent.py
+++ projects/smartcab/smartcab/agent.py
@@ -101,7 +101,10 @@ class LearningAgent(Agent):
         # Set the agent state and default action
         self.state = state
         self.next_waypoint = self.planner.next_waypoint()
-        action = None
+        # action = None
+        action = random.choice([None, 'left', 'right', 'forward'])
+
+
 
         ########### 
         ## TO DO ##
